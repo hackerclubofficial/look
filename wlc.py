0a3a2259787e83ff50c3abcb08d582f6b790457c
hackerclubofficial <83916648+hackerclubofficial@users.noreply.github.com>
2021-05-11 07:36:05 +0530

    Add files via upload

--- wlc.py
+++ wlc.py
@@ -1,20 +1,10 @@
 import os,sys,time,random
-
 print("")
-
 print("")
-
 color = ["\033[1;31m","\033[1;32m"]
-
 m = random.choice(color)+"Welcome HackerClub \n"
-
 for msg in m:
-
     sys.stdout.write(msg)
-
     sys.stdout.flush()
-
     time.sleep(0.06)
-
 print("")
-
